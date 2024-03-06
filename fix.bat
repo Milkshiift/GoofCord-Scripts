d0b437b61bbf7a396973a1f100d24443a7c8c822
Milkshift <99212308+Milkshiift@users.noreply.github.com>
2024-03-06 13:11:15 +0200

    Delete fix.bat

--- fix.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-:: Windows script to copy patches into the scripts folder
-
-set "source_patches=.\patches"
-set "destination_goofmod=C:\Users\Administrator\AppData\Roaming\GoofCord\scripts"
-
-:: Copy all files from the "patches" folder
-xcopy "%source_patches%" "%destination_goofmod%" /E /Y
